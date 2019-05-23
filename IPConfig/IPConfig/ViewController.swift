3df2a13a83b97db2c53f8ee40355a655a4f3c195
吴伟鑫 <wuweixin@4399inc.com>
2019-05-23 16:17:57 +0800

    创建IPConfig项目

---
+++ IPConfig/IPConfig/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IPConfig
+//
+//  Created by wuweixin on 2019/5/23.
+//  Copyright © 2019 cn.wessonwu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
