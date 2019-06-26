52d283318231a7f48594182c0b5e6a258d37be3b
吴伟鑫 <wuweixin@4399inc.com>
2019-06-26 19:47:52 +0800

    旋转变换处理

--- CircleSlider/CircleSlider/ParentLocker.swift
+++ CircleSlider/CircleSlider/ParentLocker.swift
@@ -8,7 +8,67 @@
 
 import UIKit
 
+final class ParentLockerLayer: CircleSliderLayer {
+    override func drawTrackPath(in ctx: CGContext) {
+        super.drawTrackPath(in: ctx)
+        
+        ctx.saveGState()
+        
+        let color = UIColor.valueOf(rgb: 0xC3C3C3)
+        // 指示方向
+        let arcCenter = CGPoint(x: bounds.midX, y: bounds.midY)
+        let halfOfPi = CGFloat.pi / 2
+        let startAngle = -halfOfPi + CGFloat(0.1) * 2 * CGFloat.pi
+        let endAngle = startAngle + CGFloat(0.28) * 2 * CGFloat.pi
+        let ovalPath = UIBezierPath(arcCenter: arcCenter,
+                                    radius: trackRadius,
+                                    startAngle: startAngle,
+                                    endAngle: endAngle,
+                                    clockwise: true)
+        
+        ctx.addPath(ovalPath.cgPath)
+        ctx.setLineWidth(2)
+        ctx.setLineDash(phase: 0, lengths: [10, 2])
+        ctx.setStrokeColor(color.cgColor)
+        ctx.strokePath()
+        ctx.restoreGState()
+        
+        
+        ctx.saveGState()
+        // 箭头 (paincode)
+        let arrowPath = UIBezierPath()
+        arrowPath.move(to: CGPoint(x: 6.5, y: 0))
+        arrowPath.addLine(to: CGPoint(x: 0, y: 12))
+        arrowPath.addLine(to: CGPoint(x: 6.5, y: 8)) //以该点为锚点
+        arrowPath.addLine(to: CGPoint(x: 13, y: 12))
+        arrowPath.addLine(to: CGPoint(x: 6.5, y: 0))
+        arrowPath.close()
+        
+        let alpha = halfOfPi - endAngle
+        let offsetX = trackRadius * cos(alpha)
+        let offsetY = trackRadius * sin(alpha)
+        let boundsOfArrow = arrowPath.bounds
+        var transform = CGAffineTransform.identity.translatedBy(x: -6.5, y: -8)
+        transform = transform.translatedBy(x: arcCenter.x, y: arcCenter.y).translatedBy(x: offsetX, y: offsetY)
+        let centerOfArrow = CGPoint(x: boundsOfArrow.midX, y: boundsOfArrow.midY)
+        transform = transform.translatedBy(x: centerOfArrow.x, y: centerOfArrow.y)
+                            .rotated(by: CGFloat.pi + endAngle)
+                            .translatedBy(x: -centerOfArrow.x, y: -centerOfArrow.y)
+        arrowPath.apply(transform)
+        
+        
+        
+        ctx.addPath(arrowPath.cgPath)
+        ctx.setFillColor(color.cgColor)
+        ctx.fillPath(using: .evenOdd)
+        
+        ctx.restoreGState()
+    }
+}
+
+
 final class ParentLocker: CircleSlider {
+    override class var layerClass: Swift.AnyClass { return ParentLockerLayer.self }
     
     lazy var imageView = UIImageView(image: #imageLiteral(resourceName: "ic_radish"))
     
@@ -65,5 +125,17 @@ extension UIColor {
     
     class func valueOf(rgb: UInt32) -> UIColor {
         return valueOf(argb: rgb | 0xFF000000)
+    }
+}
+
+
+fileprivate extension UIBezierPath {
+    func applyCentered(transform:CGAffineTransform) {
+        let center = PathBoundingCenter(path: path)
+        var t = CGAffineTransform.identity
+        t = t.translatedBy(x: center.x, y: center.y)
+        t = transform.concatenating(t)
+        t = t.translatedBy(x: -center.x, y: -center.y)
+        path.apply(t)
     }
 }
