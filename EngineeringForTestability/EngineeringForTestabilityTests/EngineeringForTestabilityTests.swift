642ffe5f9fa2451fa05957d750f7d7f578e28ba6
wessonwu <wessonwu94@gmail.com>
2019-08-19 22:36:27 +0800

    EngineeringForTestability

---
+++ EngineeringForTestability/EngineeringForTestabilityTests/EngineeringForTestabilityTests.swift
@@ -0,0 +1,38 @@
+//
+//  EngineeringForTestabilityTests.swift
+//  EngineeringForTestabilityTests
+//
+//  Created by wuweixin on 2019/8/19.
+//  Copyright © 2019 wuweixin. All rights reserved.
+//
+
+import XCTest
+@testable import EngineeringForTestability
+
+class EngineeringForTestabilityTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        
+        let viewController = ViewController()
+        viewController
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
