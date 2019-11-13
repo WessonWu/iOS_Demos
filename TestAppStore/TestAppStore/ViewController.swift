d670f224aca2ad3efb8a92d3fff70cccbb455f02
吴伟鑫 <wuweixin@4399inc.com>
2019-11-13 16:36:20 +0800

    项目：TestAppStore
    基本要求：
    - AppIcon (每个图片都要，且需要符合要求的大小)
    - Info.plist中添加：<key>ITSAppUsesNonExemptEncryption</key><false/>   (防止审核时提示缺少出口合规证明)

---
+++ TestAppStore/TestAppStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestAppStore
+//
+//  Created by wuweixin on 2019/11/13.
+//  Copyright © 2019 wuweixin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
