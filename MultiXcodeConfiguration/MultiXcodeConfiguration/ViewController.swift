406cb7368c4c461fff46e080a36a60083b129be5
wessonwu <wessonwu94@gmail.com>
2019-12-16 23:43:21 +0800

    configuration setup

--- MultiXcodeConfiguration/MultiXcodeConfiguration/ViewController.swift
+++ MultiXcodeConfiguration/MultiXcodeConfiguration/ViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var networkLabel: UILabel!
+    @IBOutlet weak var productLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        #if PRODUCTION
+        productLabel.text = "Production"
+        #endif
+        
+        #if DEVELOPMENT
+        productLabel.text = "Development"
+        #endif
+        
+        #if TEST
+        networkLabel.text = "Test"
+        #endif
+        
+        #if ONLINE
+        networkLabel.text = "Online"
+        #endif
+        
+        request(URL(string: "https://help.apple.com/xcode/mac/10.2/#/dev745c5c974")!, method: .get, parameters: nil, encoding: URLEncoding(), headers: nil).responseData { (resp) in
+            print(resp)
+        }
+        
+        print(Bundle.main.bundleIdentifier)
     }
 
 
