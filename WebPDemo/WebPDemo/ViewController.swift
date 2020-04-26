ea5163c8acdfbdca72458a0684cb5cd215f0b824
吴伟鑫 <wuweixin@4399inc.com>
2020-04-26 15:45:08 +0800

    add client load webp image support demo

--- WebPDemo/WebPDemo/ViewController.swift
+++ WebPDemo/WebPDemo/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import Kingfisher
+import KingfisherWebP
+
+/**
+ Reference from https://developers.google.com/speed/webp/gallery?hl=zh-cn
+ webp url: https://www.gstatic.com/webp/gallery/4.sm.webp?dcb_=0.40009090765093513
+ */
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        // request webp
+        let options: KingfisherOptionsInfo = [
+            .processor(WebPProcessor.default),
+            .cacheSerializer(WebPSerializer.default)
+        ]
+        self.imageView.kf.setImage(
+            with: URL(string: "https://www.gstatic.com/webp/gallery/4.sm.webp?dcb_=0.40009090765093513"),
+            options: options
+        )
     }
 
 
